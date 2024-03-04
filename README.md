407e4fbdf715d36054c5c68c81409aaba5919979
wam <wampfler@gmail.com>
2024-03-04 12:44:26 +0100

    setup and led blink

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # midipedal
+
+## Links
+- https://www.instructables.com/DIY-USB-Midi-Controller-With-Arduino-a-Beginners-G/
+- https://docs.arduino.cc/tutorials/generic/midi-device/
+- https://github.com/tttapa/Control-Surface
--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,15 +1,40 @@
 /*
 Board: todo
 Hardware: todo
-Libraries: todo
+Libraries: MIDIUSB, ezButton, MagicPot
 */
 
-void setup() {
-  // put your setup code here, to run once:
+#define CHANNEL = 1
 
+void setup()
+{
+  Serial.begin(9600);
+
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
 
+  // x = map(x, 0, 1024, 0, 127);
+  // x = constrain(x, 0, 127)
+}
+
+void controlChange(byte control, byte value)
+{
+  Serial.print("send MIDI CC control ");
+  Serial.print(control);
+  Serial.print(" = ");
+  Serial.println(value);
+
+  // First parameter is the event type (0x0B = control change).
+  // Second parameter is the event type, combined with the channel.
+  // Third parameter is the control number number (0-119).
+  // Fourth parameter is the control value (0-127).
+  // midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+  // MidiUSB.sendMIDI(event);
 }
